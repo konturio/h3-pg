a9f035b01ea91b2ae68de8ab145735185317516d
sergei sh <sshoulbakov@kontur.io>
2022-08-29 20:49:45 +0300

    Split h3 polygons by 180th meridian

--- h3_postgis/sql/updates/h3_postgis--4.0.0--unereleased.sql
+++ h3_postgis/sql/updates/h3_postgis--4.0.0--unereleased.sql
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2022 Bytes & Brains
+ * Copyright 2018-2022 Bytes & Brains
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,35 +14,17 @@
  * limitations under the License.
  */
 
---| # PostGIS Indexing Functions
+-- complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use "ALTER EXTENSION h3_postgis UPDATE TO 'unreleased'" to load this file. \quit
 
---@ availability: 4.0.0
-CREATE OR REPLACE FUNCTION h3_lat_lng_to_cell(geometry, resolution integer) RETURNS h3index
-    AS $$ SELECT h3_lat_lng_to_cell($1::point, $2); $$ IMMUTABLE STRICT PARALLEL SAFE LANGUAGE SQL;
-
---@ availability: 4.0.0
-CREATE OR REPLACE FUNCTION h3_lat_lng_to_cell(geography, resolution integer) RETURNS h3index
-    AS $$ SELECT h3_lat_lng_to_cell($1::geometry, $2); $$ IMMUTABLE STRICT PARALLEL SAFE LANGUAGE SQL;
-
---@ availability: 4.0.0
-CREATE OR REPLACE FUNCTION h3_cell_to_geometry(h3index) RETURNS geometry
-  AS $$ SELECT ST_SetSRID(h3_cell_to_lat_lng($1)::geometry, 4326) $$ IMMUTABLE STRICT PARALLEL SAFE LANGUAGE SQL;
-
---@ availability: 4.0.0
-CREATE OR REPLACE FUNCTION h3_cell_to_geography(h3index) RETURNS geography
-  AS $$ SELECT h3_cell_to_geometry($1)::geography $$ IMMUTABLE STRICT PARALLEL SAFE LANGUAGE SQL;
-
---@ availability: unreleased
 CREATE OR REPLACE FUNCTION
     h3_cell_to_boundary_wkb(cell h3index, split_at_meridian boolean DEFAULT FALSE) RETURNS bytea
 AS 'h3' LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE; COMMENT ON FUNCTION
     h3_cell_to_boundary_wkb(h3index, boolean)
 IS 'Finds the boundary of the index, returns EWKB, second argument splits polygon when crossing 180th meridian';
 
---@ availability: 4.0.0
 CREATE OR REPLACE FUNCTION h3_cell_to_boundary_geometry(h3index, split boolean DEFAULT FALSE) RETURNS geometry
   AS $$ SELECT ST_SetSRID(h3_cell_to_boundary_wkb($1, $2)::geometry, 4326) $$ IMMUTABLE STRICT PARALLEL SAFE LANGUAGE SQL;
 
---@ availability: 4.0.0
 CREATE OR REPLACE FUNCTION h3_cell_to_boundary_geography(h3index, split boolean DEFAULT FALSE) RETURNS geography
-  AS $$ SELECT h3_cell_to_boundary_geometry($1, $2)::geography $$ IMMUTABLE STRICT PARALLEL SAFE LANGUAGE SQL;
+  AS $$ SELECT h3_cell_to_boundary_geometry_wkb($1, $2)::geography $$ IMMUTABLE STRICT PARALLEL SAFE LANGUAGE SQL;
